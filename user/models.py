000627b60bce1730e197ffc9dc8ffdf392f8ea5f
Daniel Ortiz <thisisdanielortiz@gmail.com>
2022-09-02 13:52:49 -0500

    constraints and migrations

--- user/models.py
+++ user/models.py
@@ -11,8 +11,12 @@ https://adamj.eu/tech/2021/02/26/django-check-constraints-prevent-self-following
 # Create your models here.
 class User(AbstractUser):
     email = models.EmailField(unique=True, max_length=255)
-    following = models.ManyToManyField(to='self', related_name='following_list', null=True, blank=True)
-    followers = models.ManyToManyField(to='self', related_name='followers_list', null=True, blank=True)
+    followers = models.ManyToManyField(
+        to='self',
+        through='Follow',
+        related_name='following_list',
+        symmetrical=False
+        )
     profile_photo = models.ImageField(upload_to='static/', blank=True)
     birthday = models.DateField(null=True, blank=True, default=None)
     bio = models.CharField(max_length=160, null=True, blank=True, default=None)
@@ -24,6 +28,21 @@ class User(AbstractUser):
             models.CheckConstraint(check=models.Q(birthday__lte=(datetime.date.today() - datetime.timedelta(days=6570))), name='is_adult')
         ]
 
+class Follow(models.Model):
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='origin_user')
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='destination_user')
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                name='unique_relationships',
+                fields=['from_user', 'to_user']
+            ),
+            models.CheckConstraint(
+                name='prevent_self_follow',
+                check=~models.Q(from_user=models.F("to_user"))
+            )
+        ]
 
     def __str__(self):
         return self.username
