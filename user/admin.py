929c53560a11a79ce134cc26438873c90a96380d
Daniel Ortiz <thisisdanielortiz@gmail.com>
2022-09-06 11:42:47 -0500

    models and relationships

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import User, Follow
+from .models import User, Follow, Post, Comment
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Follow)
+admin.site.register(Post)
+admin.site.register(Comment)
